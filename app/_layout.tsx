967efc2817379816172e4b8857c9c14ad29c7a86
Bolshiyanov <bolshiyanov@gmail.com>
2023-08-22 19:19:06 +0100

    Finaly export

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,10 @@
-import FontAwesome from "@expo/vector-icons/FontAwesome";
+
 import {
   DarkTheme,
   DefaultTheme,
   ThemeProvider,
 } from "@react-navigation/native";
-import { useFonts } from "expo-font";
-import { SplashScreen, Stack } from "expo-router";
-import { useEffect } from "react";
+import {  Stack } from "expo-router";
 import { useColorScheme } from "react-native";
 
 export const unstable_settings = {
@@ -14,8 +12,6 @@ export const unstable_settings = {
   initialRouteName: "(tabs)",
 };
 
-// Prevent the splash screen from auto-hiding before asset loading is complete.
-SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
